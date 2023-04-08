6b8ea4e6bcdd36b9a5ea36071c3f1e09ed83ed4a
saurav <sauravvermas1097v@gmail.com>
2023-04-08 16:12:21 +0530

    LCA

---
+++ 17LowestCommonAnscestor.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int n){
+        data = n;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printLevelOrder(Node *root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* node = q.front();                                 
+        q.pop();
+        if(node != NULL){
+            cout<<node->data<<" ";  
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }                                           
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+            // cout<<endl;
+        }
+    }
+}
+
+Node* LCA(Node *root, int n1, int n2){
+    if(root == NULL){
+        return NULL;
+    }
+    if(root->data == n1 || root->data == n2){
+        return root;
+    }
+    Node *leftAns = LCA(root->left, n1, n2);
+    Node *rightAns = LCA(root->right, n1, n2);
+
+    if(leftAns != NULL && rightAns != NULL){
+        return root;
+    }
+    else if(leftAns == NULL && rightAns != NULL){
+        return rightAns;
+    }
+    else if(leftAns != NULL && rightAns == NULL){
+        return leftAns;
+    }
+    else{
+        return NULL;
+    }
+
+}
+
+int main(){
+    struct Node* root = new Node(20); 
+    root->left = new Node(8);
+    root->right = new Node(22);
+    root->right->right = new Node(25);
+    root->left->left = new Node(4);
+    root->left->right = new Node(12);
+    root->left->right->left = new Node(10);
+    root->left->right->right = new Node(14);
+
+    cout<<"Levelorder is: "<<endl;
+    printLevelOrder(root);
+    cout<<endl;
+
+    cout<<"The Lowest common ancestor of 10 and 14 is: "<<endl;
+    Node *temp = LCA(root, 10, 14);
+    cout<<temp->data<<endl;
+   
+
+    return 0;
+}
+/* 
+
+          20      Not Balanced
+        /    \       
+       8     22             
+     /  \       \
+    4     12     25
+         /   \   
+      10     14
+*/   
+   
+   
+   
+   
+    
--- Rough.cpp
+++ Rough.cpp
@@ -1,110 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int n){
-        data = n;
-        left = NULL;
-        right = NULL;
-    }
-};
-
-void preOrder(struct Node* root){
-   stack<Node*> st;
-    vector<int> ans;
-    st.push(root);
-    // Node *node = root;
-    while(!st.empty()){
-        root = st.top();
-        st.pop();
-        ans.push_back(root->data);
-        if(root->right){
-            st.push(root->right);
-        }
-        if(root->left){
-            st.push(root->left);
+void removeSpecialCharacter(string s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] < 'A' || s[i] > 'Z' && s[i] < 'a' || s[i] > 'z')
+        {
+            // cout << i << " ";
+            s.erase(i, 1);
+            i--;
         }
     }
-    for(int i=0; i<ans.size()-1; i++){
-        cout<<ans[i]<<" ";
+    cout<<s<<endl;
+    string s1 = "";
+    for (int i = 0; i <= s.size(); i++)
+    {
+        s1 = s1 + s[s.size()-i];
+    }
+    cout<<s1<<endl;
+
+    if(s.compare(s1)==0){
+        cout<<"T"<<endl;
+    }
+    else{
+        cout<<"F"<<endl;
     }
 }
 
-void inOrder(struct Node* root){
-    stack<Node*> st;
-    vector<int> ans;
-    st.push(root);
-    Node *node = root;
-    while(!st.empty()){
-        if(node != NULL){
-            st.push(node);
-            node = node->left;
-        }
-        else{
-            if(st.empty()){
-                break;
-            }
-            node = st.top();
-            st.pop();
-            ans.push_back(node->data);
-            node = node->right;
-        }
-    }
-    for(int i=0; i<ans.size()-1; i++){
-        cout<<ans[i]<<" ";
-    }
-}
-void postOrder(struct Node* root){
-    stack<Node*> st;
-    vector<int> ans;
-    st.push(root);
-    while(!st.empty()){
-        root = st.top();
-        ans.push_back(root->data);
-        if(root->left){
-            st.push(root->left);
-        if(root->right){
-            st.push(root->right);
-        }
-        }
-    }
-    for(int i=0; i<ans.size()-1; i++){
-        cout<<ans[i]<<" ";
-    }
-}
-
-int main(){
-    struct Node* root = new Node(1); 
-    root->left = new Node(2);
-    root->right = new Node(3);
-
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
-
-    cout<<"Preorder Using Stack is: "<<endl;
-    preOrder(root);
-    cout<<endl;
-    cout<<"Inorder Using Stack is: "<<endl;
-    inOrder(root);
-    // cout<<endl;
-    // cout<<"Postorder Using Stack is: "<<endl;
-    // postOrder(root);
-
+int main()
+{
+    string s = "race a car";
+    // string s = "A man, a plan, a canal: Panama";
+    removeSpecialCharacter(s);
+    // string str = "Journal Dev reverse example";
+    // reverse(str.begin(), str.end());
+    // cout<<"\n"<<str;
     return 0;
 }
-
-/* 
-          1
-        /   \
-       2     3
-      / \   / \
-     4   5 6   7
-
-*/
