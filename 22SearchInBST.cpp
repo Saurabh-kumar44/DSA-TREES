09de367893e56391baaf53a333f3565830437c0f
saurav <sauravvermas1097v@gmail.com>
2023-04-19 14:18:39 +0530

    SEarching in BST iterative

--- 22SearchInBST.cpp
+++ 22SearchInBST.cpp
@@ -1,93 +1,121 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 
-    Node(int n){
+    Node(int n)
+    {
         data = n;
         left = NULL;
         right = NULL;
     }
 };
 
-void printLevelOrder(Node *root){
-    queue<Node*> q;
+void printLevelOrder(Node *root)
+{
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
-    while(!q.empty()){
-        Node* node = q.front();                                 
+    while (!q.empty())
+    {
+        Node *node = q.front();
         q.pop();
-        if(node != NULL){
-            cout<<node->data<<" ";  
-            if(node->left){
+        if (node != NULL)
+        {
+            cout << node->data << " ";
+            if (node->left)
+            {
                 q.push(node->left);
             }
-            if(node->right){
+            if (node->right)
+            {
                 q.push(node->right);
-            }                                           
+            }
         }
-        else if(!q.empty()){
+        else if (!q.empty())
+        {
             q.push(NULL);
             // cout<<endl;
         }
     }
 }
 
-//Here searching taking O(log(n))
-Node *searchInBST(Node *root , int k){
-    if(root == NULL){
-        return NULL;
+// Here searching taking O(log(n)) //Recursive
+//  Node *searchInBST(Node *root , int k){
+//      if(root == NULL){
+//          return NULL;
+//      }
+//      if(root->data == k){
+//          return root;
+//      }
+//      if(root->data > k){
+//          Node* left = searchInBST(root->left, k);
+//          return left;
+//      }
+//      else{
+//          Node* right = searchInBST(root->right, k);
+//          return right;
+//      }
+//  }
+
+//Iterative searching
+bool searchInBST(Node *root, int k)
+{
+    Node *temp = root;
+    while (temp != NULL){
+        if(temp->data == k){
+            return true;
+        }
+        if(temp->data > k){
+            temp = temp->left;
+        }
+        else{
+            temp = temp->right;
+        }
     }
-    if(root->data == k){
-        return root;
-    }
-    if(root->data > k){
-        Node* left = searchInBST(root->left, k);
-        return left;
-    }
-    else{
-        Node* right = searchInBST(root->right, k);
-        return right;
-    }
+    return false;
 }
 
-
-int main(){
-    struct Node* root = new Node(4); 
+int main()
+{
+    struct Node *root = new Node(4);
     root->left = new Node(2);
     root->right = new Node(5);
     root->right->right = new Node(6);
     root->left->left = new Node(1);
     root->left->right = new Node(3);
 
-    cout<<"Levelorder is: "<<endl;
+    cout << "Levelorder is: " << endl;
     printLevelOrder(root);
-    cout<<endl;
+    cout << endl;
 
-    Node *temp = searchInBST(root, 6);
-    if(temp==NULL){
-        cout<<"Key doesn't exist"<<endl;
+    // Node *temp = searchInBST(root, 69);
+    // if(temp == NULL){
+    //     cout << "Key doesn't exist" << endl;
+    // }
+    // else{
+    //     cout << "Key is present" << endl;
+    // }
+
+    bool temp = searchInBST(root, 6);
+    if (temp){
+        cout << "Key is present" << endl;
     }
     else{
-        cout<<"Key is present"<<endl;
+        cout << "Key doesn't exist" << endl;
     }
-   
 
     return 0;
 }
-/* 
+/*
 
-          4     
-         /  \    
-        2    5             
+          4
+         /  \
+        2    5
        /  \   \
       1    3   6
-*/   
-   
-   
-   
-   
-    
+*/
